ed852305aa024c418d161a96ff35ae60b2a04d96
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-02 00:57:58 +0200

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -75,7 +75,7 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-6"></div>
-            <div className="col-lg-6 col-md-6">
+            <div className="col-lg-6 col-md-6 text-end ">
               <img src={countImg} alt="" />
             </div>
           </div>
