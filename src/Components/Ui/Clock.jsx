c1907f26bef03d36cd405e6b76a9015b9d3d68aa
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-02 01:36:00 +0200

    update

--- src/Components/Ui/Clock.jsx
+++ src/Components/Ui/Clock.jsx
@@ -1,5 +1,42 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 export default function Clock() {
-  return <div>Clock</div>;
+  const [days, setdays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+  let interval;
+  const countDown = () => {};
+  return (
+    <>
+      <div className="colorWrapper d-flex align-items-center gap-3">
+        <div className="clockData d-flex align-items-center gap-3">
+          <div className="text-center">
+            <h4 className="h1 text-white fs-3 mb-2">10</h4>
+            <h5 className="text-white fs-6">Days</h5>
+          </div>
+          <span className=" text-white fs-3">:</span>
+        </div>
+        <div className="clockData d-flex align-items-center gap-3">
+          <div className="text-center">
+            <h4 className="h1 text-white fs-3 mb-2">10</h4>
+            <h5 className="text-white fs-6">Hours</h5>
+          </div>
+          <span className=" text-white fs-3">:</span>
+        </div>
+        <div className="clockData d-flex align-items-center gap-3">
+          <div className="text-center">
+            <h4 className="h1 text-white fs-3 mb-2">10</h4>
+            <h5 className="text-white fs-6">Minutes</h5>
+          </div>
+          <span className=" text-white fs-3">:</span>
+        </div>
+        <div className="clockData d-flex align-items-center gap-3">
+          <div className="text-center">
+            <h4 className="h1 text-white fs-3 mb-2">10</h4>
+            <h5 className="text-white fs-6">Seconds</h5>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
