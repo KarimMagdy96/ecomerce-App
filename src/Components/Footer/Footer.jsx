115530e77822a4ec094f79f39efdf7d0a88fb417
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-02 02:26:23 +0200

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return <div className="">Footer</div>;
+  return (
+    <>
+      <footer className="footer">
+        <dv className="container">
+          <div className="row">
+            <div className="col-lg-4"></div>
+            <div className="col-lg-3"></div>
+            <div className="col-lg-2"></div>
+            <div className="col-lg-3"></div>
+          </div>
+        </dv>
+      </footer>
+    </>
+  );
 }
