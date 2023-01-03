4358353e15aab86b437ff07375b3a4bd43c8cde1
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-03 10:50:06 +0200

    update

--- src/pages/Prodactdetails/Prodactdetails.jsx
+++ src/pages/Prodactdetails/Prodactdetails.jsx
@@ -1,9 +1,9 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { useParams } from "react-router-dom";
 import products from "../../assets/data/products";
 import Helmet from "../../Components/Helmet/Helmet";
 import CommonSection from "../../Components/Ui/CommonSection";
-
 export default function Prodactdetails() {
   const { id } = useParams();
   const product = products.find((item) => item.id === id);
@@ -12,14 +12,14 @@ export default function Prodactdetails() {
     productName,
     price,
     avgRating,
-    review,
+    reviews,
     discription,
     shortDesc,
   } = product;
   return (
     <>
-      <Helmet title="Products Details">
-        <CommonSection />
+      <Helmet title={productName}>
+        <CommonSection title={productName} />
         <section className="pt-5">
           <div className="container">
             <div className="row">
@@ -53,7 +53,24 @@ export default function Prodactdetails() {
                   </div>
                   <span className="productPrice">${price}</span>
                   <p className="mt-3">{shortDesc}</p>
-                  <button className="buyBtn">Add To Cart</button>
+                  <motion.button whileTap={{ scale: 1.2 }} className="buyBtn">
+                    Add To Cart
+                  </motion.button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="tabWraaper d-flex align-items-center gap-5">
+                  <h6>Description</h6>
+                  <h6>Reviews ({reviews.length})</h6>
+                </div>
+                <div className="tabContent mt-5">
+                  <p>{discription}</p>
                 </div>
               </div>
             </div>
