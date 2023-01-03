2afefe93843dfd2d6949625187cbb4ba4ec6f823
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-03 02:33:16 +0200

    update

--- src/Components/Ui/CommonSection.jsx
+++ src/Components/Ui/CommonSection.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function CommonSection() {
-  return <div>CommonSection</div>;
+  return (
+    <>
+      <section className="commonSection">
+        <div className="container text-center">
+          <h1>{title}</h1>
+        </div>
+      </section>
+    </>
+  );
 }
