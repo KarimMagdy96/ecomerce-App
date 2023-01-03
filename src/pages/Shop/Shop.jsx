148fe8660c944ebee0cd08b18d73c6a2c7b4cbb2
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-03 06:21:23 +0200

    update

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,11 +2,43 @@ import React, { useState } from "react";
 import CommonSection from "../../Components/Ui/CommonSection";
 import products from "../../assets/data/products";
 import Helmet from "../../Components/Helmet/Helmet";
+import ProductsList from "../../Components/Ui/ProductsList";
 export default function Shop() {
   const [productsData, setProductsData] = useState(products);
   const handelFilter = (e) => {
     const filterValue = e.target.value;
     if (filterValue === "sofa") {
+      const filterProducts = products.filter(
+        (item) => item.category === "sofa"
+      );
+      setProductsData(filterProducts);
+    }
+    if (filterValue === "mobile") {
+      const filterProducts = products.filter(
+        (item) => item.category === "mobile"
+      );
+      setProductsData(filterProducts);
+    }
+    if (filterValue === "wireless") {
+      const filterProducts = products.filter(
+        (item) => item.category === "wireless"
+      );
+      setProductsData(filterProducts);
+    }
+    if (filterValue === "chair") {
+      const filterProducts = products.filter(
+        (item) => item.category === "chair"
+      );
+      setProductsData(filterProducts);
+    }
+    if (filterValue === "watch") {
+      const filterProducts = products.filter(
+        (item) => item.category === "watch"
+      );
+      setProductsData(filterProducts);
+    }
+    if (filterValue === "") {
+      setProductsData(products);
     }
   };
   return (
@@ -17,17 +49,20 @@ export default function Shop() {
           <div className="row">
             <div className="col-lg-3 col-md-3">
               <div className="filterWidget">
-                <select>
-                  <option value="sofa">Filter By Category</option>
+                <select onChange={handelFilter}>
+                  <option disabled selected>
+                    Filter By Category
+                  </option>
+                  <option value="">All</option>
                   <option value="sofa">Sofa</option>
                   <option value="mobile">Mobile</option>
-                  <option value="Chair">Chair</option>
-                  <option value="Watch">Watch</option>
-                  <option value="Wireless">Wireless</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
                 </select>
               </div>
             </div>
-            <div className="col-lg-3 col-md-3 ">
+            <div className="col-lg-3 col-md-3">
               <div className="filterWidget">
                 <select>
                   <option value="sofa">Sort By </option>
@@ -45,6 +80,15 @@ export default function Shop() {
           </div>
         </div>
       </section>
+      <div className="container pb-3 pt-0">
+        <div className="row">
+          {productsData.length === 0 ? (
+            <div className="w-100 vh-100">NO Products Found !</div>
+          ) : (
+            <ProductsList data={productsData} />
+          )}
+        </div>
+      </div>
     </Helmet>
   );
 }
