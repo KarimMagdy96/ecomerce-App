f82f4731c9d92af4d5aea5433ac58968fa1d3b75
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-03 06:50:45 +0200

    update

--- src/pages/Prodactdetails/Prodactdetails.jsx
+++ src/pages/Prodactdetails/Prodactdetails.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import products from "../../assets/data/products";
+import Helmet from "../../Components/Helmet/Helmet";
+import CommonSection from "../../Components/Ui/CommonSection";
 
 export default function Prodactdetails() {
-  return <div>Prodactdetails</div>;
+  const { id } = useParams();
+  const product = products.find((item) => item.id === id);
+  const { imgUrl, ProdactName, price, avgRating, review, discription } =
+    product;
+  return (
+    <>
+      <Helmet title="Products Details">
+        <CommonSection />
+        <section className="pt-0">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <img src={imgUrl} alt="product" />
+              </div>
+              <div className="col-lg-6"></div>
+            </div>
+          </div>
+        </section>
+      </Helmet>
+    </>
+  );
 }
