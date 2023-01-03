65ca268d5c193ea715992468927d954c1bea5b41
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-03 03:10:08 +0200

    update

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CommonSection from "../../Components/Ui/CommonSection";
+
 import Helmet from "../../Components/Helmet/Helmet";
 export default function Shop() {
   return (
@@ -11,15 +12,30 @@ export default function Shop() {
             <div className="col-lg-3 col-md-3">
               <div className="filterWidget">
                 <select>
+                  <option value="sofa">Filter By Category</option>
                   <option value="sofa">Sofa</option>
                   <option value="mobile">Mobile</option>
                   <option value="Chair">Chair</option>
                   <option value="Watch">Watch</option>
+                  <option value="Wireless">Wireless</option>
                 </select>
               </div>
             </div>
-            <div className="col-lg-3 col-md-3"></div>
-            <div className="col-lg-6 col-md-6"></div>
+            <div className="col-lg-3 col-md-3">
+              <select>
+                <option value="sofa">Sort By </option>
+                <option value="ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+              </select>
+            </div>
+            <div className="col-lg-6 col-md-6">
+              <div className="searchBox">
+                <input type="text" placeholder="Search...." />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </section>
