670079291fa7287cb03f287345cbc77cec5c348d
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2023-01-06 20:01:19 +0200

    update

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,44 @@
 import React from "react";
-
+import Helmet from "../../Components/Helmet/Helmet";
+import CommonSection from "../../Components/Ui/CommonSection";
 export default function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <>
+      <Helmet title="CheckOut">
+        <CommonSection title="CheckOut" />
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <h6 className="mb-4 fw-bold">Billing Information</h6>
+                <form className="billingForm">
+                  <div className="formGroup">
+                    <input type="text" placeholder="Enter Your Name" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="email" placeholder="Enter Your Email" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="number" placeholder="Enter Your Phone" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="text" placeholder="Street Address" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="text" placeholder="City" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="text" placeholder="Postal Code" />
+                  </div>
+                  <div className="formGroup">
+                    <input type="text" placeholder="Country" />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Helmet>
+    </>
+  );
 }
